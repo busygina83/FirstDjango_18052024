e88c4dd2bac883805a2b4370aeb204d1be5e329d
busygina83 <busygina83@mail.ru>
2024-05-25 14:03:03 +0300

    Task2

--- MainApp/views.py
+++ MainApp/views.py
@@ -23,14 +23,6 @@ def home(request):
     }
     return render(request, "index.html", context)
 
-def item_details(request,item_id):
-    for item in items:
-        if item['id']==item_id:
-            return render(request, "item.html", {"item": item})
-    return HttpResponseNotFound(
-        f"""<p>Товар с id={item_id} не найден<br>
-        <a href="/items">Назад к списку товаров</a></p>""")
-
 # def home(request):
 #     fio="Бусыгина И.В."
 #     text = f"""
@@ -50,24 +42,25 @@ def about(request):
     return HttpResponse(text)
 
 def item_list(request):
-    text = "<strong>Список товаров:</strong><br>"
-    for item in items:
-        text += f"""<p>
-            {item['id']}: {item['name']}
-            <a href="/item/{item['id']}"> подробнее...</a>
-            </p>"""
-    return HttpResponse(text)
+    # for item in items:
+    #     text += f"""<p>
+    #         {item['id']}: {item['name']}
+    #         <a href="/item/{item['id']}"> подробнее...</a>
+    #         </p>"""
+    # return HttpResponse(text)
+    return render(request, "items.html", {"items": items})
 
-# def item_details(request, item_id):
-#     for item in items:
-#         if item['id']==item_id:
-#             text = f"""<p>
-#                 id: <strong>{item['id']}</strong><br>
-#                 name: <strong>{item['name']}</strong><br>
-#                 quantity: <strong>{item['quantity']}</strong><br>
-#                 <a href="/items">Назад к списку товаров</a><br>
-#                 </p>"""
-#             return HttpResponse(text)
-#     return HttpResponseNotFound(
-#         f"""<p>Товар с id={item_id} не найден<br>
-#         <a href="/items">Назад к списку товаров</a></p>""")
+def item_details(request, item_id):
+    for item in items:
+        if item['id']==item_id:
+            # text = f"""<p>
+            #     id: <strong>{item['id']}</strong><br>
+            #     name: <strong>{item['name']}</strong><br>
+            #     quantity: <strong>{item['quantity']}</strong><br>
+            #     <a href="/items">Назад к списку товаров</a><br>
+            #     </p>"""
+            # return HttpResponse(text)
+            return render(request, "item.html", {"item": item})
+    return HttpResponseNotFound(
+        f"""<p>Товар с id={item_id} не найден<br>
+        <a href="/items">Назад к списку товаров</a></p>""")
