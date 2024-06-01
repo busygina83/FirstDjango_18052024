becba7d24db8d651b98cda3a70f4351826900a9a
busygina83 <busygina83@mail.ru>
2024-06-01 10:31:37 +0300

    HomeWork2

--- FirstDjango/settings.py
+++ FirstDjango/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     BASE_DIR / "static",
