e39d61ea964703f4114917f2c2e07d1b18a2b108
busygina83 <busygina83@mail.ru>
2024-06-05 23:33:18 +0300

    Task4

--- SnippetsTemplate/Snippets/settings.py
+++ SnippetsTemplate/Snippets/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
   BASE_DIR / "static"
 ]
